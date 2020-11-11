00620409a0f271e66a1393fa21a9ca5d92f6a28d
Ata Gowani <atagowani@gmail.com>
2020-11-11 16:55:15 -0600

    Set up basic routing structure

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+# set the "static" directory as the static folder
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost")
