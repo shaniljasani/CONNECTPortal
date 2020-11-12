cbd0a4e1a360456ba6c09237ce0a745cca522f0c
Ata Gowani <atagowani@gmail.com>
2020-11-11 18:33:33 -0600

    Add sample AirTable call

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+import requests
+import os
+import urllib.parse
+
+from dotenv import load_dotenv
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+load_dotenv(dotenv_path="./config.py")
 
 # set the "static" directory as the static folder
 app = Flask(__name__, static_url_path='/static')
+
+API_KEY=os.getenv("AIRTABLE_API_KEY")
+
+def get_some_data():
+    BASE_ID = "appm9g9Ewv87tg8al" # Add base id of AirTable application
+    TABLE_NAME = "Feedback Session"
+
+    response = requests.get('https://api.airtable.com/v0/' + BASE_ID + '/' + urllib.parse.quote(TABLE_NAME), headers={'Authorization': 'Bearer ' + API_KEY})
+    data = response.json()
+
+    print(data)
 
 @app.route('/<path:path>')
 def send_static(path):
@@ -14,4 +31,5 @@ def hello():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost")
+    get_some_data()
+    # app.run(debug=True, host="localhost")
