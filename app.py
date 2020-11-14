f933eac5b575eefde6a3a9add64c7b71d7464c15
Ata Gowani <atagowani@gmail.com>
2020-11-14 11:42:51 -0600

    Fix links to views and assets (#2)
    * Add all the available routes
    
    * Add logout route
    
    * Update all links and assets
    
    * Update links
    
    * Update links
    
    * Update links
    
    * Update links

--- app.py
+++ app.py
@@ -13,5 +13,25 @@ def send_static(path):
 def hello():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("login.html")
+
+@app.route('/resources')
+def resources():
+    return render_template("resources.html")
+
+@app.route('/schedules')
+def schedules():
+    return render_template("schedules.html")
+
+@app.route('/forgot-password')
+def forgotPassword():
+    return render_template("forgot-password.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
