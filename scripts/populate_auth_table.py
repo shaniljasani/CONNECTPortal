6d5633e5d3a4d00f2bbd3cadb59e6acef74f871c
Ata Gowani <atagowani@gmail.com>
2020-11-21 16:16:06 -0600

    Add script to populate airtable auth table

---
+++ scripts/populate_auth_table.py
@@ -0,0 +1,76 @@
+import os
+
+from dotenv import load_dotenv
+from airtable import Airtable
+
+load_dotenv(dotenv_path="../config.py")
+
+API_KEY = os.getenv("AIRTABLE_API_KEY")
+BASE_ID = os.getenv("BASE_ID")
+
+auth_table = Airtable(BASE_ID, "Authentication", API_KEY)
+participant_table = Airtable(BASE_ID, "Participant", API_KEY)
+facilitator_table = Airtable(BASE_ID, "Facilitator", API_KEY)
+
+participants = dict()
+
+print("Getting ids from participant table...")
+
+participants_data = participant_table.get_all(fields="ID")
+# Go through participant table data and store in participants with ID = airtable_id
+for participant in participants_data:
+  fields = participant["fields"]
+  if fields:
+    id = fields["ID"]
+    if id:
+      participants[id] = participant["id"]
+
+records_count = len(participants)
+print(str(records_count) + " records taken from participant table.")
+
+print("Getting records from auth table...")
+
+records_count = 0
+authentication_data = auth_table.get_all(fields="ID")
+# Go through authentication data and find duplicates with participant table
+for participant in authentication_data:
+  fields = participant["fields"]
+  if fields:
+    id = fields["ID"]
+    if id:
+      participants[id] = None
+      records_count += 1
+
+print(str(records_count) + " records already exist in authentication table.")
+
+records = [[]]
+batch = 0
+record_iteration = 0
+new_records = 0
+
+# Create batch records
+for participant_id, reference_id in participants.items():
+  # If 10 records have already been added to a batch
+  if record_iteration == 10:
+    record_iteration = 0
+    records.append([])
+    batch += 1
+
+  if reference_id:
+    new_records += 1
+
+    records[batch].append({
+      "ID": participant_id,
+      "Password": "Apple",
+      "Participant_Ref": [
+        reference_id
+      ]
+    })
+    record_iteration += 1
+
+print("Adding records to authentication table...")
+
+for batch in records:
+  auth_table.batch_insert(batch)
+
+print(str(new_records) + " new records added to the authentication table.")
